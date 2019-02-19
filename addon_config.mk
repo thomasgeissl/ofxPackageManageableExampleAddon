9b68ef5cdedb9ca034a2cef4d1bbf43a9dd875f1
Thomas Geissl <thomas.geissl@gmail.com>
2019-02-18 20:22:08 -0400

    added same format as in addons.make

--- addon_config.mk
+++ addon_config.mk
@@ -9,4 +9,5 @@ common:
 	ADDON_DEPENDENCIES = ofxIO ofxMidi@1.1.1
 	ADDON_DEPENDENCIES += https://github.com/thomasgeissl/ofxLiveSet.git@de988236fcc2c152cea0d08f27ff1685e11bf920
 	ADDON_DEPENDENCIES += npisanti/ofxPDSP@e438d5269b3d53c46536904c5eb90f9a7ad05c61
+	ADDON_DEPENDENCIES += ofxEasing #https://github.com/arturoc/ofxEasing.git@3a15beffb9cfdce26ffafb1f78e06e730b26c239
 	
